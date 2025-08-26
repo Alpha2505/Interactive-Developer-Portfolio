b5c7db0c6d3ca84c383a4c8ee4c806435c1da940
Alpha2505 <gthnjlr@gmail.com>
2025-08-26 13:53:38 -0400

    Updated the repo name

--- src/app/portfolio-details/portfolio-details.component.ts
+++ src/app/portfolio-details/portfolio-details.component.ts
@@ -53,7 +53,7 @@ export class PortfolioDetailsComponent implements OnInit {
         this.category = 'Web Application';
         this.type  = 'Personal Project';
         this.date = 'August, 2025';
-        this.URL = 'https://github.com/Alpha2505/my-portfolio';
+        this.URL = 'https://github.com/Alpha2505/Interactive-Developer-Portfolio';
         this.description = "Interactive Developer Portfolio is a personal/professional website that showcases an individual's work, skills, accomplishments, and experiences. It is a static website developed using Angular. A Bootstrap template is seamlessly integrated into the Angular project, ensuring a visually appealing and responsive design. Angular routing is implemented for a smooth and intuitive multipage website experience. The Portfolio is successfully hosted on GitHub Pages, showcasing a user-friendly interface for professional presentation. ";
        }
        else if(this.id=='P5'){
